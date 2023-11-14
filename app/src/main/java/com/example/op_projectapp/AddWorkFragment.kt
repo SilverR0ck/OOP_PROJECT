2c18f07516373fe4e314c9a1e9ab323e56279e05
Lee-EunSeok <lanmecan@naver.com>
2023-11-14 13:49:47 +0900

    AddWorkFragment_update

--- app/src/main/java/com/example/op_projectapp/AddWorkFragment.kt
+++ app/src/main/java/com/example/op_projectapp/AddWorkFragment.kt
@@ -5,21 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
+import androidx.fragment.app.viewModels
 import com.google.firebase.Firebase
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.database
 
 
 class AddWorkFragment : Fragment() {
 
-    // 데이터베이스 참조
-    private val database = Firebase.database
-    private val WorkRef = database.getReference("places")
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    //viewModel 객체 생성
+    private val viewModel: PlaceViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,15 +25,19 @@ class AddWorkFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_addwork, container, false)
 
         val work_placenameEditText = view.findViewById<EditText>(R.id.workplacename)
-        val register_Button = view.findViewById<EditText>(R.id.registerButton)
+        val salary_EditText = view.findViewById<EditText>(R.id.wageday)
+        val register_Button = view.findViewById<Button>(R.id.registerButton)
+
 
 
         register_Button.setOnClickListener{
             val workPlaceName = work_placenameEditText.text.toString()
+            val salary = salary_EditText.text.toString()
             //Place객체에 근무지 이름, 월급, 시간 등 데이터를 넘겨서 객체 생성
-            val add_Work = Place(workPlaceName)
-            // 생성된 객체를 데이터 베이스에 저장
-            WorkRef.setValue(add_Work)
+
+
+            val place = Place(workPlaceName, salary)
+            viewModel.addPlace(place)
 
         }
 
--- app/src/main/java/com/example/op_projectapp/PlaceViewModel.kt
+++ app/src/main/java/com/example/op_projectapp/PlaceViewModel.kt
@@ -7,12 +7,13 @@ import com.example.op_projectapp.repository.PlaceRepository
 
 data class Place(
     val name: String = "",
-    val salary: Int = 0
+    val salary: String = "0"
 )
+
 
 class PlaceViewModel : ViewModel() {
     private val _nameplace = MutableLiveData<List<Place>>()
-    val nameplace: LiveData<List<Place>> get() = _nameplace
+    val nameplace: LiveData<List<Place>> get() = _nameplace //외부로부터 안전하게 접근
 
     private val repository = PlaceRepository()
 
@@ -20,9 +21,12 @@ class PlaceViewModel : ViewModel() {
         repository.observePlaces(_nameplace)
     }
 
+
     fun addPlace(place: Place) {
         val newList = _nameplace.value?.toMutableList() ?: mutableListOf()
         newList.add(place)
         repository.postPlaces(newList)
     }
+
+
 }
