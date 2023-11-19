d7c5dceda526810dd7ebd6b9a3c1b2c353042763
Lee-EunSeok <lanmecan@naver.com>
2023-11-19 23:51:24 +0900

    "ChangeWork modify"

--- app/src/main/java/com/example/op_projectapp/PlaceViewModel.kt
+++ app/src/main/java/com/example/op_projectapp/PlaceViewModel.kt
@@ -7,16 +7,17 @@ import com.example.op_projectapp.repository.PlaceRepository
 import java.util.UUID
 
 data class Place(
-    val id: String = UUID.randomUUID().toString(),
+    val id: String = UUID.randomUUID().toString(), // UUID를 사용하여 고유한 ID를 생성
     val name: String = "스타벅스",
-    //val id: String = UUID.randomUUID().toString(),// UUID를 사용하여 고유한 ID를 생성
     val wageday: String = "1",
     val wageamount: String = "0",
     val starttime: String = "0",
     val endtime: String = "24",
-    val salary: Int = 0,
+    val salary: Int? = 0,
     val daycount: Int = 0,
-    val dayCalendarCheck: List<Int> = mutableListOf()
+    val dayCalendarCheck: List<Int> = mutableListOf(),
+   // val workDays: List<Boolean> = List(7) { false },  // 월요일부터 일요일까지 각 요일에 대해 일하는지 여부
+
 )
 
 class PlaceViewModel : ViewModel() {
@@ -34,4 +35,5 @@ class PlaceViewModel : ViewModel() {
         newList.add(place)
         repository.postPlaces(newList)
     }
+
 }
